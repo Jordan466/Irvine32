d108137a08b5ea3f766ddbabe64a0aebc426435c
Jordan <jordan.warren@outlook.com.au>
2024-07-18 14:28:01 +0800

    fix

--- ch03/AddSub.asm
+++ ch03/AddSub.asm
@@ -9,7 +9,7 @@ INCLUDE Irvine32.inc
 
 .code
 main PROC
-    mov eax, 1000h      ; Move hexadecimal value 1000h into EAX
+    mov eax, 10000h      ; Move hexadecimal value 1000h into EAX
     add eax, 40000h     ; Add hexadecimal value 40000h to EAX
     sub eax, 20000h     ; Subtract hexadecimal value 20000h from EAX
     call DumpRegs       ; Call DumpRegs to display the contents of registers
